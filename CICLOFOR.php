4eaf88182bc626c2a6d94cf3a185356411d4d054
sistemas <you@example.com>
2022-07-18 16:21:48 -0500

    Tutorial PHP

---
+++ CICLOFOR.php
@@ -0,0 +1,40 @@
+<?php
+
+    //ciclo for ... 
+    /*
+    echo "hola Kevin";
+    echo "hola Kevin";
+    echo "hola Kevin";
+    echo "hola Kevin";
+    echo "hola Kevin";
+    echo "hola Kevin";
+    echo "hola Kevin";
+    echo "hola Kevin";
+    echo "hola Kevin";
+    echo "hola Kevin";
+    */
+
+    // Hola Kevin
+    for ($i=0; $i<=10; $i++) {
+       echo "Hola Kevin <br/>";
+    }
+
+    // numeros del 1 al 20
+    for ($numero=1; $numero<=20; $numero++) {
+        echo $numero . "<br/>";
+    }
+
+    // numeros del 1 al 30 de 2 en 2
+    for ($numero=1; $numero<=30; $numero=$numero+2) {
+        echo $numero . "<br/>";
+    }
+
+echo "-----------------------";
+
+    // numeros del 1 al 50 de 2 en 2
+    for ($numero=1; $numero<=50; $numero+=2) {
+        echo $numero . "<br/>";
+    }
+ 
+
+?>
---
+++ VARIABLES.php
@@ -0,0 +1,18 @@
+<?php
+    // Esto es un comentario...
+
+    // mi primera imprecion
+    echo "Hola Kevin";
+    echo "<br>";
+
+    $nombre = "Kevin";
+    $apellido = "Arguello";
+    $edad = "17";
+
+
+    // concatenacion
+    $imprimir = $nombre ." ". $apellido . " " . $edad;
+    echo $imprimir;
+
+
+?>
